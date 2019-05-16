861cbc9dc822e5151546ff607cbc2a149c983cbb
meteor.kor <meteor.kor@gmail.com>
2019-05-16 16:09:29 +0900

    first

---
+++ src/main/java/com/meteor/SpringStudy1905Application.java
@@ -0,0 +1,13 @@
+package com.meteor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy1905Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudy1905Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/meteor/SpringStudy1905ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.meteor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringStudy1905ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
